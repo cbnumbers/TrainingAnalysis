de650f8e89d63bac6af7dce3c0a9c132f3cf9937
cbnumbers <cboutte91@gmail.com>
2018-04-24 22:18:31 -0600

    Add ts and Plot

--- Prep2018Analysis.R
+++ Prep2018Analysis.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(googlesheets)
+library(ggplot2)
 library(xts)
+library(lubridate)
 
 gs_ls()
 
@@ -9,8 +11,23 @@ PrepStats2018 <- gs_title("PrepStats2018")
 Phase1 <- gs_read(PrepStats2018, ws = "Phase1Stats")
 Phase2 <- gs_read(PrepStats2018, ws = "Phase2Stats")
 
-FullData <- Phase1 %>%
-  union(Phase2)
+FullData <- gs_read(PrepStats2018, ws = "FullData") 
+
+FullData <- FullData %>%
+  mutate(Day = as.Date(FullData$Day, "%m/%d/%Y"))
+
+ts <- FullData %>%
+  xts(order.by = FullData$Day)
+
+lm <- 
+
+ggplot(FullData, aes(x = Day, y = Weight)) +
+  geom_point(aes(col = Phase )) +
+  geom_smooth(aes(color = Phase))
+
+autoplot(ts, geom = "point")
+  
+  
 
 Day <- as.Date(Phase1$Day)
 timeseries <- xts(x = Phase1, order.by = as.Date(PhasDay)
